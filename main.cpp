c691d26104d2269a9809d63e622ca6df8810a18b
abrahamc72 <52030144+abrahamc72@users.noreply.github.com>
2020-09-14 18:09:53 -0700

    Add files via upload
    Added left shift

--- main.cpp
+++ main.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
+#include <iostream>
+#include <iostream>
 #include <fstream>
+#include <cstring>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+string shiftLeft(string s)
+{
+    string result;
+    for (int i = 1; i < s.size(); ++i) {
+        result += s[i];
+    }
+    result += s[0];
+    return result;
+}
+
 
 int main()
 {
-    fstream input;
-    input.open ("haiku.txt");
-    char test[] =
+    string test = "Mississippi";
+    int rows = sizeof(test);
+    string array[rows];
+    string shiftedarray;
+
+    shiftedarray = shiftLeft(test);
+
+
+
+    std::cout << shiftedarray << std::endl;
 
 
 }
+
+
+
+
+
+
+
